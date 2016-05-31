6672bbc821c475fc4ad4b35fd853aee419f3e742
Chris Keyes <chuy5685@yahoo.com>
2016-05-31 13:39:13 -0600

    Continuing to add code. No changes to existing code.

--- MainR-Code.R
+++ MainR-Code.R
@@ -2,16 +2,47 @@
 # Functions and Programs will be called from folder Script
 # SEM data is within the Data folder
 
+#Load functions
+source("~/SEM_Project/Script/getParkData.R")
+source("~/SEM_Project/Script/getParkName.R")
+
 
 # Convert .xls data to .csv
+### do we want to automate this within VBA?
 
 # Import data
-CUVA_dataCSV <- read.csv("~/SEM_Project/Parks/CUVA/Data/CUVA_dataCSV.csv")
+# Set working directory to the data folder
+setwd("~/SEM_Project/Data")
 
-# List variable names
-Varnames <- colnames(CUVA_dataCSV)
+# Save list of park datasets in Data folder
+Parklist <- list.files()
+  Parklist
+
+# Loop over all datasets and change name to four letter name
+# and create a vector of dataset names called "DataNameVector"
+DataNameVector <- c(NULL)
+for (x in Parklist) {
+    ParkName <- substr(x, 1, 4)
+    DataNameVector <- append(DataNameVector, ParkName)
+    
+        WD <- getwd()
+        filename <- paste(WD, x,sep = "/")
+    
+    park <- read.csv(filename)
+    getParkName(ParkName)
+        park <- NULL
+}
+
+
+# Choose data set to work with 
+###Should we work within a large loop??
+DataNameVector  #If we loop, can use this vector of park names
+
+Varnames <- colnames(CUVA)
     Varnames
     
 # Subset data on selected varnames
 
 CUVA_data1 <- CUVA_dataCSV[,c(1:5,7)] #how do we want to break up varnames? 
+
+
