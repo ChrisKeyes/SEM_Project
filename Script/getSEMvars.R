080eec1ab5f78c56ac2ae0b65714458282762d64
Chris Keyes <chuy5685@yahoo.com>
2016-05-31 18:16:43 -0600

    This script is not working yet. May not even be necessary.

--- Script/getSEMvars.R
+++ Script/getSEMvars.R
@@ -1,6 +1,14 @@
-# function SEMvars creates a vector of vectors which each dataset has called Vars
+# function SEMvars creates a vector of variable names which each dataset has called Vars
 # and creates a vector of variable names which the park data does not have called noVars
 # and lastly creates a vector of variable names which are essential to spending effect
 # analysis but are specific to this park and creates a vector of these variable names 
 # called NewVars
 
+getSEMvars <- function(SearchVars){
+
+  
+for (x in SearchVars){
+  TempVars <- Varnames[grepl( x ,Varnames)]
+  Vars <- append(Vars, TempVars)
+    }
+}
