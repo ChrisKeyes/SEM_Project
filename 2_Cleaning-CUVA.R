ab66a4de0ed33a647eeec45eaf6aa3627a384e70
Chris Keyes <chuy5685@yahoo.com>
2016-06-09 15:28:06 -0600

    Working script, but not finished

--- 2_Cleaning-CUVA.R
+++ 2_Cleaning-CUVA.R
@@ -1,37 +1,82 @@
 # THis script will use a series of conditional statements to identify missing, incomplete, and outlying observations.
 # 
-# Will use "PARKsem" as general name for the dataset
+# Use the script 1_Upload... to get the data
+setwd("~/SEM_Project")
+source("1_Load&Subset-CUVA.R")
 
+# Clear the environment except for the data we want
+rm(list=setdiff(ls(), "CUVAsem"))
+
+# Will use "PARKsem" as general name for the dataset
 PARKsem <- CUVAsem
 
 ###################################################################################################
+# Upload the CHECKvars csv and store as "CHECKvars"
+CHECKvars <- read.csv("~/SEM_Project/CHECKVars.csv")
+    CHECKvars <- CHECKvars[,c("SEMvars","NumberChecks")]  #drop the notes column
+      row.names(CHECKvars) <- CHECKvars$SEMvars
 
-# NEED TO DO A CHECK TO VERIFY THE VARIABLES MATCH
+# Drop SEMvars which PARKsem does not contain
+PARKvars <- colnames(PARKsem[which(colnames(PARKsem) != c("ID"))])
+    PARKvars <- as.factor(PARKvars)
+    
+CHECKvars <- CHECKvars[c(PARKvars),] 
+    # row.names(CHECKvars) <- NULL
 
-CHECKvars <- read.csv("~/SEM_Project/SEMvars.csv")
-  CHECKvars
 
 # Create Data frame for taxonomy of bads
-
 Bads <- data.frame(PARKsem[,c("ID")])
   colnames(Bads) <- c("ID")
   
-# Use the variable names in PARKsem as list of checks and loop over each ID
+
+# y <- "HoursinPark"
+CHECKS <- NULL  
+  for (y in CHECKvars$SEMvars){
+    z <- CHECKvars[c(y),c("NumberChecks")]
+      if (is.na(z) == "TRUE"){
+        #do nothing, there is no check on this variable
+      }
+      else { #the number of checks will be the number of columns added
+        CHECKS <- append(CHECKS, y) 
+         for (n in 1:z){
+            ck = paste( y , n, sep = "_") 
+            Bads[, ck] <- 0
+          }
+      }
+  }
   
-Checks <- colnames(PARKsem[which(colnames(PARKsem) != c("ID"))])
+####################################################################################################
 
-Bads$LiveNearbyNA <- 0
+# Each column of Bads (except "ID") is a check
 
-# this will work for the outer loop
-for (x in levels(PARKsem$ID)){
-  ID1 <- x
-}
+# HoursinPark; check for NA
+check <- PARKsem[is.na(PARKsem$HoursinPark),"ID"]
+    Bads$HoursinPark_1[c(check)] <- 1
 
-# this will work for generating a data frame to fill with results
-for (y in Checks){
-  ck = y
-    Bads[, ck] <- 0
-}
+# LiveNearby; check for NA
+check <- PARKsem[is.na(PARKsem$LiveNearby),"ID"]
+    Bads$LiveNearby_1[c(check)] <- 1
 
-ID1 <- PARKsem$ID[1]
+# Overnight_1; check for NA
+check <- PARKsem[is.na(PARKsem$Overnight),"ID"]
+    Bads$Overnight_1[c(check)] <- 1
+    
+
+# DaysinPark_1; check for DaysinPark > 14   (Outlier test)
+    
+    
+# a_Zip_1; check for NULL
+    
+    
+# a_Zip_2; check for string length < 5 charracters (incomplete zip code)
+    
+    
+# a_Zip_3; check for non-numeric (international) 
+    
+    
+    
+    
+    
+  
+  
 
