2b99b9cfebe64cddf11ac56f47499db9d3f28252
Chris Keyes <chuy5685@yahoo.com>
2016-06-14 16:04:31 -0600

    Almost done!

--- 2_Cleaning-CUVA.R
+++ 2_Cleaning-CUVA.R
@@ -14,37 +14,44 @@ PARKsem <- CUVAsem
 # Upload the CHECKvars csv and store as "CHECKvars"
 CHECKvars <- read.csv("~/SEM_Project/CHECKVars.csv")
    ### CHECKvars <- CHECKvars[,c("SEMvars","NumberChecks")]  #drop the notes column
-      row.names(CHECKvars) <- CHECKvars$SEMvars
+   ###row.names(CHECKvars) <- CHECKvars$SEMvars
 
 # Drop SEMvars which PARKsem does not contain
 PARKvars <- colnames(PARKsem[which(colnames(PARKsem) != c("ID"))])
-    PARKvars <- as.factor(PARKvars)
-    
-CHECKvars <- CHECKvars[c(PARKvars),] 
-    # row.names(CHECKvars) <- NULL
+   ###PARKvars <- as.factor(PARKvars)
 
+Matches <- NULL    
+for (v in PARKvars){
+  MM <-   grep( v, CHECKvars$SEMvars, ignore.case = TRUE, value = FALSE)
+  Matches <- append(Matches, MM)
+}
+
+CHECKvars <- data.frame(CHECKvars[Matches,])
+    # row.names(CHECKvars) <- NULL
+    # head(CHECKvars)
 
 # Create Data frame for taxonomy of bads
 Bads <- data.frame(PARKsem[,c("ID")])
   colnames(Bads) <- c("ID")
   
 
-# y <- "HoursinPark"
+ 
 CHECKS <- NULL  
-  for (y in CHECKvars$SEMvars){
-    z <- CHECKvars[c(y),c("NumberChecks")]
-      if (is.na(z) == "TRUE"){
-        #do nothing, there is no check on this variable
-      }
-      else { #the number of checks will be the number of columns added
-        CHECKS <- append(CHECKS, y) 
-         for (n in 1:z){
-            ck = paste( y , n, sep = "_") 
-            Bads[, ck] <- 0
-          }
-      }
+for (y in 1:length(CHECKvars$SEMvars)){
+  z <- CHECKvars[c(y),c("NumberChecks")]
+  if (is.na(z) == "TRUE"){
+    #do nothing, there is no check on this variable
   }
-  
+  else { #the number of checks will be the number of columns added
+    CHECKS <- append(CHECKS, CHECKvars$SEMvars[y]) 
+    for (n in 1:z){
+      ck = paste( CHECKvars$SEMvars[y] , n, sep = "_") 
+      Bads[, ck] <- 0
+    }
+  }
+}
+
+ 
 ####################################################################################################
 
 # Each column of Bads (except "ID") is a check
@@ -63,17 +70,116 @@ check <- PARKsem[is.na(PARKsem$Overnight),"ID"]
     
 
 # DaysinPark_1; check for DaysinPark > 14   (Outlier test)
+    ### STILL NEED TO SORT THIS OUT
+ 
+#***************************************************************************************************
+# a_Zip:Factor variable with 306 levels
+# The as.numeric function forces the factors into numbers with range 1:number of levels
+    # For a_Zip, as.numeric identifies each a_Zip as a number 1:306
+        levels(PARKsem$a_Zip)   
+        
+# Examining levels we have:
+        # 1 = " " (Null or blank entries)
+        # 301:306 = All countries and non-zip codes
+        
+# a_Zip_1; Identify NULL (blank zip codes)
+check <- PARKsem[as.numeric(PARKsem$a_Zip) == 1 ,"ID"]
+      Bads$a_Zip_1[c(check)] <- 1
+        
+        
+# a_Zip_2; Identify Countries;    FOR CUVA 301:306 -- WILL NEED TO AUTOMATE OR SPECIFY THESE VALUES FOR EACH PARK
+check <- PARKsem[as.numeric(PARKsem$a_Zip) == 301 |
+                   as.numeric(PARKsem$a_Zip) == 302 | 
+                   as.numeric(PARKsem$a_Zip) == 303 |
+                   as.numeric(PARKsem$a_Zip) == 304 |
+                   as.numeric(PARKsem$a_Zip) == 305 |
+                   as.numeric(PARKsem$a_Zip) == 306 ,"ID"]    # "|" is the "or" syntax
+
+      Bads$a_Zip_2[c(check)] <- 1
+
+# a_Zip_3: Identify incomplete zip codes    ******NOT WORKING
+    # check <- PARKsem[length(PARKsem$a_Zip) < 5, "ID"]
+    #       Bads$a_Zip_3[c(check)] <- 1
     
+
+#***************************************************************************************************
+# Check for NA's in expenditure categories:
+#     GasCleaned
+#     RentalCar
+#     PublicTransportation
+#     Resturaunts
+#     Snacks
+#     Groceries
+#     Hotels
+#     EquipmentRental
+#     SpecialtyLodge
+#     Camping
+#     Recreation
+#     Souvenirs
+#     Guide
+#     Railroad
+#     Tours
+#     OtherExp
+#     AdditionalExpen
+
+# Create vector of expenditure variable names
+ExpVars <- c("GasCleaned", "RentalCar","PublicTransportation", "Resturaunts", "Snacks", 
+             "Groceries", "Hotels", "EquipRental", "Recreation", "Souvenirs",
+             "Guide", "Railroad", "Tours", "OtherExpen", "AdditionalExpen")
+
+# Loop through the variables in ExpVars
+# If the given expenditure variable exists within PARKsem then check for NA's within that column
+# Save the ID which corresponds to the NA and for that ID number, save a "1" in the Bads data frame
+# under the column named "ExpVars_1" (where ExpVars is the current variable name in the loop)
+for (y in ExpVars){
+  if (exists(y, where = PARKsem) == TRUE){
     
-# a_Zip_1; check for NULL
+    check <- PARKsem[is.na(PARKsem[c(y)]),"ID"]
     
+    v = paste(y , "1", sep = "_")
+    Bads[c(check), c(v)] <- 1
+  }
+}
+
+#***************************************************************************************************
+# Check for NA's in Segment categories:
+# Overnight_1: Check for NA
+# ReEnter_1: check for NA
+# CampingBackcountry_1: Check for NA
+# CampinginPark_1; Check for NA
+# CampingOutPark_1: Check for NA
+# LodgingInPark_1: Check for NA
+# LodgingOutPark_1: Check for NA
+# HoursinPark_1: Check for NA
+
+SegmentVars <- c("Overnight", "ReEnter", "CampingBackcountry", "CampingInPark",
+                 "CampingOutPark", "LodgingInPark", "LodgingOutPark", "HoursinPark")    
+
+for (y in SegmentVars){
+  if (exists(y, where = PARKsem) == TRUE){
     
-# a_Zip_2; check for string length < 5 charracters (incomplete zip code)
+    check <- PARKsem[is.na(PARKsem[c(y)]),"ID"]
     
-    
-# a_Zip_3; check for non-numeric (international) 
-    
-    
+    v = paste(y , "1", sep = "_")
+    Bads[c(check), c(v)] <- 1
+  }
+}
+
+
+#***************************************************************************************************
+
+# ReEnter_2: check for ReEnter == 0
+
+# DKReEnter_1: 
+
+
+
+
+# DaysinPark_1: Check for outliers (DaysinPark > 14)
+
+
+
+      
     
     
     
