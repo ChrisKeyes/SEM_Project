0f64b4b018ee3feae2f4f4414db65715c71d3c7a
Chris Keyes <chuy5685@yahoo.com>
2016-06-21 13:59:52 -0600

    Working.

--- 4_Variable-Generation.R
+++ 4_Variable-Generation.R
@@ -4,20 +4,32 @@
 # Use the preceeding scripts to get the data
 setwd("~/SEM_Project")
 
-source("1_Load&Subset.R") # Optional, can just run "2_Cleaning.R"
 source("2_Cleaning.R")
 
 # Set data frames to park of interest 
 PARKbads <- CUVAbads
 PARKsem <- CUVAsem
 
+###########################################################################################
+#***************** SEGMENT VARIABLE CREATION & ANALYSIS ***********************************
+
+
 # For segment variables, drop observations which do not match rules:
 # rule_1: Must have answered local (from PARKbads, local_1 == 0)
 # rule_2: Must have answered overnight or daysPark (from PARKbads, overnight_1 == 0 & daysPark_1 == 0)
-IDpassR1 <- PARKbads[PARKbads$local_1 == 0 , "ID"]  #rule 1
-    PARKsegments <- PARKsem[c(IDpass),] 
+# IDpassR1 <- PARKbads[PARKbads$local_1 == 0 , "ID"]  #rule 1
+#     PARKsegments <- PARKsem[c(IDpassR1),] 
+# 
+# IDpassR2 <- PARKbads[PARKbads$overnight_1 == 0 &
+#                      PARKbads$daysPark_1 == 0 , "ID"] #rule 2
+# 
+# PARKsegments <- PARKsegments[na.omit(match(IDpassR2, PARKsegments$ID)),]
 
-IDpassR2 <- PARKbads[PARKbads$overnight_1 == 0 &
-                     PARKbads$daysPark_1 == 0 , "ID"] #rule 2
+# Try to drop observations, rather than keep
+r1Fail <- PARKbads[PARKbads$local_1 == 1 , "ID"]  #rule 1
+PARKsegments <- PARKsem[-c(r1Fail),] 
+
+IDpassR2 <- PARKbads[PARKbads$overnight_1 == 1 &
+                       PARKbads$daysPark_1 == 1 , "ID"] #rule 2
 
 PARKsegments <- PARKsegments[na.omit(match(IDpassR2, PARKsegments$ID)),]
