4fc3da5d642b5fc2028c7ac0755e1487a4792cf2
CathyCThomas <ccullinanethomas@usgs.gov>
2016-06-27 13:46:09 -0600

    Added example code for the apply function

--- PracticeFiles/playingWithTheData.R
+++ PracticeFiles/playingWithTheData.R
@@ -33,5 +33,14 @@ for (x in 1:length(CUVAsem$ID)){
 ReEnterNAs <-sum(is.na(CUVAsem$ReEnter))
 ReEnterNAs
 
+#Example of the apply function
+## Compute row and column sums for a matrix:
+x <- cbind(x1 = 3, x2 = c(4:1, 2:5))
+dimnames(x)[[1]] <- letters[1:8]
+apply(x, 2, mean, trim = .2)
+col.sums <- apply(x, 2, sum)
+row.sums <- apply(x, 1, sum)
+rbind(cbind(x, Rtot = row.sums), Ctot = c(col.sums, sum(col.sums)))
+
 
 
