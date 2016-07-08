d1204430ccdc556ece9aa8981b413ee94ff4f686
Chris Keyes <chuy5685@yahoo.com>
2016-07-08 09:56:27 -0600

    Cleaned up script layout and added a few lines.

--- Script/TripToArea2.R
+++ Script/TripToArea2.R
@@ -7,8 +7,19 @@ PARKsem <- TESTdata
 PARK <- "TEST"
 
 ###build a subset dataframe from PARKsem ##########################################
-
-FullTripToAreaVars <- c("ID","hoursPark", "daysPark","overnight","nightsCampIn","nightsCampOut","nightsLodgeIn","nightsLodgeOut","nightsCruise","nightsOther","tripPurpose","equallyNearby","primaryNearby")
+          # SegmentVars   <- c("nightsCampIn","nightsCampOut","nightsLodgeIn","nightsLodgeOut",
+          #                    "nightsCruise","nightsOther")
+          # 
+          # Trip2AreaVars <- c("ID","hoursPark","daysPark",
+          #                    "overnight","tripPurpose","equallyNearby",
+          #                    "primaryNearby")
+          # 
+          #         FullTripToAreaVars <- c(Trip2AreaVars, SegmentVars)
+          
+                  
+                  FullTripToAreaVars <- c("ID","hoursPark", "daysPark","overnight","nightsCampIn",
+                        "nightsCampOut","nightsLodgeIn","nightsLodgeOut","nightsCruise",
+                        "nightsOther","tripPurpose","equallyNearby","primaryNearby")
 TripToAreaVars <- NULL
 for (y in FullTripToAreaVars){
   if (exists(y, where = PARKsem) == TRUE){
@@ -17,13 +28,24 @@ for (y in FullTripToAreaVars){
 }
 df <- PARKsem[TripToAreaVars]
 
+          # TripToAreaVars <- c("ID","hoursPark", "daysPark","overnight","nightsCampIn","nightsCampOut","nightsLodgeIn","nightsLodgeOut","nightsCruise","nightsOther","tripPurpose","equallyNearby","primaryNearby")
+          # PARK_TripToAreaVars <- NULL
+          # 
+          # for (y in TripToAreaVars){
+          #   if (exists(y, where = PARKsem) == TRUE){
+          #     PARK_TripToAreaVars <- append(PARK_TripToAreaVars, y)
+          #   }
+          # }
+          #     df <- PARKsem[PARK_TripToAreaVars]
+
+
 #remove bads from df 
 #hoursPark_2 == 1 if hoursPark >24
 #daysPark_1 == 1 if hoursPark and daysPark were unaswered
 #daysPark_2 == 1 if daysPark > 14
 #overnight_3 == 1 if respondents answered overnight = 1 but don't list any lodging in nights*
 df$bads <- PARKbads$hoursPark_2 + PARKbads$daysPark_1 + PARKbads$daysPark_2 + PARKbads$overnight_3
-df <- subset(df, df$bads == 0)
+    df <- subset(df, df$bads == 0)
 
 
 ###Create variables for time spent in the park######################################
@@ -33,20 +55,34 @@ df <- subset(df, df$bads == 0)
 #Round partial days up to nearest whole number
 
 df$hoursParkAdj <- ifelse(df$hoursPark >0,1,0)
-df$daysParkAdj <- ifelse(!is.na(df$daysPark),df$daysPark, df$hoursParkAdj)
-df$daysParkAdj <- ceiling(df$daysParkAdj)
+    df$daysParkAdj <- ifelse(!is.na(df$daysPark),df$daysPark, df$hoursParkAdj)
+        df$daysParkAdj <- ceiling(df$daysParkAdj)
 #dfverify <- df[c("hoursPark","hoursParkAdj", "daysPark","daysParkAdj")]
 
 
 ###Create variables for time spent in local area ######################################
 
-FullLodgingTypes <- c("nightsCampIn","nightsCampOut","nightsLodgeIn","nightsLodgeOut","nightsCruise","nightsOther")
+FullLodgingTypes <- c("nightsCampIn","nightsCampOut","nightsLodgeIn","nightsLodgeOut",
+                      "nightsCruise","nightsOther")
 LodgingTypes <- NULL
 for (y in FullLodgingTypes){
   if (exists(y, where = PARKsem) == TRUE){
     LodgingTypes <- append(LodgingTypes, y)
   }
 }
+        
+        # PARK_SegmentVars <- intersect(colnames(df), SegmentVars)
+
+        # SegmentVars   <- c("nightsCampIn","nightsCampOut","nightsLodgeIn","nightsLodgeOut",
+        #                    "nightsCruise","nightsOther")
+        # PARK_SegmentVars <- NULL
+        # 
+        #   for (y in FullLodgingTypes){
+        #     if (exists(y, where = PARKsem) == TRUE){
+        #       PARK_SegmentVars <- append(PARK_SegmentVars, y)
+        #     }
+        #   }
+
 
 #create new variables nightsLocalArea and daysLocalArea that estimate time spent in the local area
 
@@ -97,38 +133,49 @@ boxplot(df$daysParkAdj,
 
 # ###Evaluate trip purpose########################################
 # 
-# #basic summary of trip purpose
-# df <- PARKsem[TripToAreaVars]
-# df <- subset(df,!is.na(df$tripPurpose))
-# tripPurpose <- as.factor(df$tripPurpose)
-# cnt <- nrow(df)
-# numPrimary <- sum(df$tripPurpose == 1)
-# perPrimary <- numPrimary / cnt 
-# numEqually <- sum(df$tripPurpose == 2)
-# perEqually <- numEqually / cnt 
-# numInci <- sum(df$tripPurpose == 3)
-# perInci <- numInci / cnt 
-# 
-# #evaluate equal purpose trips
-# #if tripPurpose = 2 and equallyNearby !is.na, want summary of 0s (not nearby) and 1s (nearby)
-# df_equally <- subset(df, df$tripPurpose == 2 & !is.na(df$equallyNearby))
-# cnt <- nrow(df_equally)
-# numEqually_Nearby <- sum(df_equally$equallyNearby == 1)
-# pEqually_Nearby <- numEqually_Nearby / cnt 
-# perEqually_Nearby <- perEqually * pEqually_Nearby
-# numEqually_NotNearby <- sum(df_equally$equallyNearby == 0)
-# pEqually_NotNearby <- numEqually_NotNearby / cnt
-# perEqually_NotNearby <- perEqually * pEqually_NotNearby
-# 
-# #evaluate incidental trips
-# #if tripPurpose = 3 and primaryNearby !is.na, want summary of 0s (not nearby) and 1s (nearby)
-# df_incidental <- subset(df, df$tripPurpose ==3 & !is.na(df$primaryNearby))
-# cnt <- nrow(df_incidental)
-# numInci_Nearby <- sum(df_incidental$primaryNearby == 1)
-# pInci_Nearby <- numInci_Nearby / cnt 
-# perInci_Nearby <- perInci * pInci_Nearby
+#basic summary of trip purpose
+df <- PARKsem[TripToAreaVars]
+    df <- subset(df,!is.na(df$tripPurpose))
+      
+tripPurpose <- as.factor(df$tripPurpose)
+    cnt <- nrow(df)
+
+numPrimary <- sum(df$tripPurpose == 1)
+    perPrimary <- numPrimary / cnt
+
+numEqually <- sum(df$tripPurpose == 2)
+    perEqually <- numEqually / cnt
+
+numInci <- sum(df$tripPurpose == 3)
+    perInci <- numInci / cnt
+
+#evaluate equal purpose trips
+#if tripPurpose = 2 and equallyNearby !is.na, want summary of 0s (not nearby) and 1s (nearby)
+df_equally <- subset(df, df$tripPurpose == 2 & !is.na(df$equallyNearby))
+    cnt <- nrow(df_equally)
+
+numEqually_Nearby <- sum(df_equally$equallyNearby == 1)
+    pEqually_Nearby <- numEqually_Nearby / cnt
+
+    perEqually_Nearby <- perEqually * pEqually_Nearby
+
+numEqually_NotNearby <- sum(df_equally$equallyNearby == 0)
+    pEqually_NotNearby <- numEqually_NotNearby / cnt
+    perEqually_NotNearby <- perEqually * pEqually_NotNearby
+
+#evaluate incidental trips
+#if tripPurpose = 3 and primaryNearby !is.na, want summary of 0s (not nearby) and 1s (nearby)
+df_incidental <- subset(df, df$tripPurpose ==3 & !is.na(df$primaryNearby))
+    cnt <- nrow(df_incidental)
+    
+numInci_Nearby <- sum(df_incidental$primaryNearby == 1)
+    pInci_Nearby <- numInci_Nearby / cnt
+        perInci_Nearby <- perInci * pInci_Nearby
+
+        
+              
 # numInci_NotNearby <- sum(df_incidental$primaryNearby == 0)
-# pInci_NotNearby <- numPrimary_NotNearby / cnt 
+# pInci_NotNearby <- numPrimary_NotNearby / cnt
 # perInci_NotNearby <- perInci * pInci_NotNearby
 # 
 # #Note: perPrimary + perEqually_Nearby +perEqually_NotNearby +perInci_Nearby+perInci_NotNearby = 1
