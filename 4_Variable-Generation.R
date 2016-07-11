686558ac26ebc518d6e8121ba9cb15019e5d7055
Chris Keyes <chuy5685@yahoo.com>
2016-07-11 13:40:30 -0600

    Script 4 now has correct segment shares and many of the modifications discussed Friday

--- 4_Variable-Generation.R
+++ 4_Variable-Generation.R
@@ -3,15 +3,14 @@
 
 # Use the preceeding scripts to get the data
 setwd("~/SEM_Project")
+
 library(plyr)
 
 source("1_Load&Subset.R")
 
-PARKsem <- TESTdata
+      PARKsem <- TESTdata
 
 source("2_Cleaning.R")
-
-
 
 PARKbads <- Bads
       rm(Bads)
@@ -46,38 +45,67 @@ PARKbads <- Bads
 PARKsegments <- PARKsem
 
 # For observations where overnight == NA, but sum(nights*)>=1, change overnight <- 1 
+    IDs_ON2 <- PARKbads[which(PARKbads$overnight_2 == 1), "ID"]   # save for testing/verification
+
 PARKsegments$overnight <- ifelse(PARKbads$overnight_2 == 1, 1, PARKsegments$overnight)
       # **** CHECK TO VERIFY THIS WORKS CORRECTLY **** 
+
+# For observations where zip == " " (i.e. zip is blank, PARKbads$zip_2 == 1), replace with zip == NA
+    IDs_ZIP2 <- PARKbads[which(PARKbads$zip_2 == 1), "ID"]
+    
+    PARKsegments$zip[c(IDs_ZIP2)] <- NA
+
+# For observations where overnight == 1, but one or more of the accomodation types is NA
+# (e.g. PARK_SegmentVars == NA), replace with zero
+for (y in PARK_SegmentVars){
+  v <- paste(y, "1", sep = "_")
+    IDs_badNights <- PARKbads[PARKbads[v]==1, "ID"]
+      PARKsegments[match(IDs_badNights, PARKsegments$ID), y] <- 0
+}
 
 # For observations where local==NA, but zip is provided, change local to 
 # local == 1 if zip matches local zipcodes or
 # local == 0 if zip matches non-local zipcodes
 
 
-PARK_localzip <- subset(PARKsem, local == 1, select = zip)
+PARK_localzip <- subset(PARKsegments, local == 1 & is.na(zip)==FALSE , select = zip)
 
-PARK_nonlocalzip <- subset(PARKsem, local == 0, select = zip)
+PARK_nonlocalzip <- subset(PARKsegments, local == 0 & is.na(zip)==FALSE , select = zip)
 
-    
+IDs_local1 <- PARKbads[which(PARKbads$local_1 == 1), "ID"]  
+          
+          # save for testing/verification
+          # IDs_local1 are observations where local == NA
+
+    # NOTE: try using dplyr "count" function rather than plry
+
     PARK_localzip <-  count(PARK_localzip)
         colnames(PARK_localzip) <- c("LOCAL_ZIP", "FREQUENCY")
             n <- sum(PARK_localzip$FREQUENCY)
             
-        LocalZip <- PARK_localzip[PARK_localzip$FREQUENCY/n > .08, "LOCAL_ZIP"]   
+        LocalZip <- PARK_localzip[PARK_localzip$FREQUENCY/n > .05, "LOCAL_ZIP"]   
                 # CHANGE WEIGHT AFTER TESTING 
 
     PARK_nonlocalzip <-  count(PARK_nonlocalzip)
         colnames(PARK_nonlocalzip) <- c("NON-LOCAL_ZIP", "FREQUENCY")
             m <- sum(PARK_nonlocalzip$FREQUENCY)
         
-        nonLocalZip <- PARK_nonlocalzip[PARK_nonlocalzip$FREQUENCY/m > .08, "NON-LOCAL_ZIP"]    
+        nonLocalZip <- PARK_nonlocalzip[PARK_nonlocalzip$FREQUENCY/m > .0909, "NON-LOCAL_ZIP"]    
       
     ErrorZip <- intersect(LocalZip, nonLocalZip) 
+          ErrorZip <- append(ErrorZip, c("DK"))  # we can add other values as needed
+          
           # The any zip values that are in the intersections must be errors
     
-        PARK_localzip <- LocalZip[which(LocalZip != ErrorZip)]
-        PARK_nonlocalzip <- nonLocalZip[which(nonLocalZip != ErrorZip)]
-
+        # PARK_localzip <- LocalZip[which(LocalZip != ErrorZip)]
+        # PARK_nonlocalzip <- nonLocalZip[which(nonLocalZip != ErrorZip)]
+                # NOTE: setdiff is the difference of set x net set y, not the union of 
+                # x & y net the intersection
+          
+        PARK_localzip <- setdiff(LocalZip, ErrorZip)
+        PARK_nonlocalzip <- setdiff(nonLocalZip, ErrorZip)
+        
+        
 for (zips in PARK_localzip){
   PARKsegments[which(PARKsegments$zip == zips & is.na(PARKsegments$local)), "local"] <- 1
 }
@@ -94,8 +122,8 @@ PARKsem_COPY <- PARKsem
 # Create a copy of original PARKsem for temporary use
 PARKsem <- PARKsegments
     
-# NOTE: AT THIS POINT, WE COULD SAVE PARKsem TO THE OUTPUT FOLDER AND CLEAN THE DATA FRAME
-# IN MEMORY USING THE SAME NAME 
+        # NOTE: AT THIS POINT, WE COULD SAVE PARKsem TO THE OUTPUT FOLDER AND CLEAN THE DATA FRAME
+        # IN MEMORY USING THE SAME NAME 
 
     source("2_Cleaning.R")
         PARKbads_cleaned <- Bads
@@ -103,13 +131,19 @@ PARKsem <- PARKsegments
             rm(Bads)
             
 # PARKsegments$sumBADS <- PARKbads$local_1 + PARKbads$overnight_1 + PARKbads$overnight_3
-PARKsegments$sumBADS <- PARKbads_cleaned$local_1 + PARKbads_cleaned$overnight_1 + PARKbads_cleaned$overnight_3
+            #  local_1: local == NA
+            # overnight_1: overnight == NA
+            # overnight_3: 
+PARKsegments$sumBADS <- PARKbads_cleaned$local_1 +
+                        PARKbads_cleaned$overnight_1 +
+                        PARKbads_cleaned$overnight_3
     
 # Create a vector of :"ID"'s which will be dropped, then subset the data by dropping bad observations
 DropIDs <- PARKsegments[PARKsegments$sumBADS >=1, "ID"]
 
 PARKsegments <- subset(PARKsegments, sumBADS == 0 , select = c(colnames(PARKsegments)))
 
+PARKbads_cleaned <- PARKbads_cleaned[-c(DropIDs),]
 
               # # Create a vector of "ID"'s which cannot be identified as local | nonlocal
               # BADlocal_1 <- PARKbads[PARKbads$local_1 == 1 , "ID"]  
@@ -137,11 +171,16 @@ PARKsegments <- subset(PARKsegments, sumBADS == 0 , select = c(colnames(PARKsegm
               #       rather than their row/column index
 
 # Reset the row numbers to a complete sequence from 1:n
-row.names(PARKsegments) <- NULL   
+          # NOTE: this may not be neccessary - factor variables maintain all associated charracteristics
+          # row.names(PARKsegments) <- NULL   
+          # row.names(PARKbads_cleaned) <- NULL
 
 # PARKsegments is now our subsetted data set (from PARKsem) that can be used for segment analysis
       head(PARKsegments)  
 
+# Clean up memory
+      rm(DropIDs, ErrorZip, LocalZip, m, MATCHvars, nonLocalZip, PARK_localzip, PARK_nonlocalzip,
+         zips)
 ###########################################################################################
 #***************** Categorize Observations by Accomodation ********************************
 
@@ -160,8 +199,10 @@ for (y in SegmentVars){
     
         PARK_SegmentVars <- append(PARK_SegmentVars, y)   # vector of segments specific to PARK for use later
       
-        v <- paste(substring(y,7),
-                   "nonlocal", sep = "_")   # This code reads:
+        v <- paste("ON", substring(y, 7), sep = "_")
+        
+        # v <- paste(substring(y,7),
+        #            "nonlocal", sep = "_")   # This code reads:
             # take the SegmentVar and remove the first six letter (e.g. "nights")
             # then combine the remaining string with "_nonlocal" and store as variable "v"
         
@@ -172,8 +213,10 @@ for (y in SegmentVars){
 
 # Identify local day trip and local overnight observations
 PARKsegments$day_local <- ifelse(PARKsegments$local == 1 & PARKsegments$overnight == 0, 1, 0)
+PARKsegments$day_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overnight == 0, 1,0)
+
 PARKsegments$overnight_local <- ifelse(PARKsegments$local == 1 & PARKsegments$overnight == 1, 1,0 )
-PARKsegments$day_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overnight == 0, 1 , 0)
+PARKsegments$overnight_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overnight == 1, 1 ,0)
 
 # PARKsegments$day_local[as.integer(PARKsegments$local)==1 &
 #                          as.numeric(PARKsegments$overnight)==0 ] <- 1
@@ -187,25 +230,28 @@ PARKsegments$day_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overn
         # identified at least one accomodation type for one or more nights, than any other accomodation types 
         # unanswered should be zero (replace NA with zero)
 
+# THIS SHOULD BE MOVED TO RUN BEFORE SCRIPT 2 IS RUN AGAIN
+
 for (y in PARK_SegmentVars){
   
   z <- paste(y, "1", sep = "_")
 
-      IDfix <- PARKbads[PARKbads[,z]==1, "ID"]
-
-          PARKsegments[c(IDfix), c(y)] <- 0
+      # IDfix <- PARKbads_cleaned[PARKbads_cleaned[,z]==1, "ID"]
+        IDfix <- PARKbads[PARKbads[,z]==1, "ID"]
+  
+          PARKsegments[match(IDfix, PARKsegments$ID), y] <- 0
 
 }
 
 # Categorize all nonlocal repondants by accomodation type. Create a vector of "ID"'s which are overnight 
 # but do not identify any type of accomodation type 
 
-badNights <- as.character(NULL) # Will capture "ID"s with incomplete data
+IDs_badNights <- as.character(NULL) # Will capture "ID"s with incomplete data
 
-for (x in 1:length(PARKsegments$local)){
-  if (as.integer(PARKsegments$local[x])== 0 &
-      as.integer(PARKsegments$overnight[x]) == 1){  # Confine loop to only nonlocal & overnight 
-          
+for (x in 1:length(PARKsegments$ID)){
+  if (as.integer(PARKsegments$overnight[x]) == 1){  # Confine loop to only overnight
+    
+
           maxNightsType <- colnames(PARKsegments[c(x),PARK_SegmentVars])[max.col(PARKsegments[c(x),PARK_SegmentVars],
                                                                                 ties.method="first")]
                 # maxNightsType: vector specifying the type of accomodation with the largest number of nights provided
@@ -218,23 +264,28 @@ for (x in 1:length(PARKsegments$local)){
           minNights <- min(PARKsegments[c(x), PARK_SegmentVars])
                 # maxNights/minNights: vector specifying the largest/smallest integer value of nights given.
           
-          if (is.na(maxNightsType) | 
+          if (is.na(maxNightsType) | # overnight == NA should be removed already (error if this is true)
               as.character(maxNights) == 0 |
               as.character(maxNights) == as.character(minNights)){ # collect "ID"'s for problematic observations 
-                    badNights <- append(badNights, as.character(PARKsegments$ID[x])) 
+                  IDs_badNights <- append(IDs_badNights, as.character(PARKsegments$ID[x])) 
           }
           
           else { # If the observation is clean, categorize it accordingly
-              v <- paste(substring(maxNightsType, 7), "nonlocal", sep = "_")
-          
-                  PARKsegments[x,v] <- 1
+              # v <- paste(substring(maxNightsType, 7), "nonlocal", sep = "_")
+                v <- paste("ON", substring(maxNightsType, 7), sep = "_")
+                  
+                PARKsegments[x,v] <- 1
         }
     }
 }
 
-PARKsegments <- PARKsegments[-c(match(badNights, PARKsegments$ID)),] # just drop these variables for the time being 
-    row.names(PARKsegments) <- NULL   # Reset row names
 
+# PARKsegments <- PARKsegments[-c(match(IDs_badNights, PARKsegments$ID)),] # just drop these variables for the time being 
+    # row.names(PARKsegments) <- NULL   # Reset row names
+
+    
+# THIS IS WHERE SCRIPT 2 SHOULD BE RERUN AND THE BADS MATRIX GENERATED
+    
 ###########################################################################################
 #***************** Generate Segment Shares ************************************************
     
@@ -250,39 +301,41 @@ PARKsegments <- PARKsegments[-c(match(badNights, PARKsegments$ID)),] # just drop
     ON_share <- n_overnight/n
     DAY_share <- n_day/n
     
-    # Sanity check
-    n_overnight == (n - n_day)   # "TRUE" = all good!
-    
-          # NOTE: think about how we want to start collecting variables such as ON_share. 
-          # Put in matrix?
-
 # Shares for day and overnight observations 
-    n_daylocal <- sum((PARKsegments[,"day_local"]==1))
-    n_daynonlocal <- sum(PARKsegments[, "day_local"]==0)
-    n_overnightlocal <- sum((PARKsegments[, "overnight_local"] == 1))
+    n_daylocal <- sum(PARKsegments[,"day_local"]==1)
+    n_daynonlocal <- sum(PARKsegments[, "day_nonlocal"]==1)
+    n_ONlocal <- sum(PARKsegments[, "overnight_local"] == 1)
+    n_ONnonlocal <- sum(PARKsegments[,"overnight_nonlocal"] == 1)
     
         DAYlocal_share <- n_daylocal/n
         DAYnonlocal_share <- n_daynonlocal/n
-        ONlocal_share <- n_overnightlocal/n
+        ONlocal_share <- n_ONlocal/n
+        ONnonlocal_share <- n_ONnonlocal/n
         
 # Get shares by accomodation type for nonlocal observations
-a <- NULL
-b <- NULL
-c <- NULL
+a <- NULL   # a: vector of variable names by segment
+b <- NULL   # b: vector of shares by segment
+c <- NULL   # c: vector of observations by segment
 
     for (y in PARK_SegmentVars){
-      v <- paste(substring(y, 7), "nonlocal", sep = "_")
-            x <- sum(PARKsegments[, v]==1)
-            z <- x/n
-                assign((paste(v, "share", sep = "_")), x )
+      v <- paste("ON", substring(y, 7), sep = "_")  # v: name of segment dummy variable
+            x <- sum(PARKsegments[, v]==1)  # x: number of observations in given segment
+            z <- x/n  # share of given segments in data set
+            
+                # assign((paste(v, "share", sep = "_")), x )
                     a <- append(a, v)
                     b <- append(b, z)
                     c <- append(c, x)
     }
 
-                    a <- append(c("Overall", "Overnight", "Day", "Day_Local", "Day_NonLocal", "Overnight_Local"), a)
-                    b <- append(c(1,ON_share, DAY_share, DAYlocal_share, DAYnonlocal_share, ONlocal_share), b)
-                    c <- append(c(n, n_overnight, n_day, n_daylocal, n_daynonlocal, n_overnightlocal), c)
+                    a <- append(c("Overall", "Overnight", "Day", "Day_Local", "Day_NonLocal",
+                                  "Overnight_Local", "Overnight_nonLocal"), a)
+                    
+                    b <- append(c(1,ON_share, DAY_share, DAYlocal_share, DAYnonlocal_share,
+                                  ONlocal_share, ONnonlocal_share), b)
+                    
+                    c <- append(c(n, n_overnight, n_day, n_daylocal, n_daynonlocal, n_ONlocal,
+                                  n_ONnonlocal), c)
 
 PARKshares_table <- data.frame(SEGMENT = a, SHARE = b, OBSERVATIONS = c)  
 
