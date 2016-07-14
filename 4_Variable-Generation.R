bd87f86363354a484d948bdf94adc328e52c23df
Chris Keyes <chuy5685@yahoo.com>
2016-07-13 18:10:21 -0600

    Looking good! Cleaned up.

--- 4_Variable-Generation.R
+++ 4_Variable-Generation.R
@@ -7,7 +7,7 @@ rm(list=union(ls(), ls()))
 # Use the preceeding scripts to get the data
 setwd("~/SEM_Project")
 
-library(plyr)
+# library(plyr)
 
 source("1_Load&Subset.R")
 
@@ -16,33 +16,7 @@ source("1_Load&Subset.R")
 # Upload the GROUPvars and create vectors of each variable category. Keep only those variables
 # that are specific to the current park.
 
-GROUPvars <- read.table("GROUPvars.csv", header = TRUE, sep = ",", na.strings = "")
-      
-  SEGMENTvars <- na.omit(GROUPvars$SEGMENTvars)
-  EXPvars <- na.omit(GROUPvars$EXPENDITUREvars)
-  TRIPvars <- na.omit(GROUPvars$TripToArea)
-  
-      PARK_SegmentVars <- NULL
-      PARK_ExpVars <- NULL
-      PARK_TripVars <- NULL
-      
-      for (VAR in SEGMENTvars){
-        if (exists(VAR, where = PARKsem) == TRUE){
-          PARK_SegmentVars <- append(PARK_SegmentVars, VAR)
-        }
-      }
-      
-      for (VAR in EXPvars){
-        if (exists(VAR, where = PARKsem) == TRUE){
-          PARK_ExpVars <- append(PARK_ExpVars, VAR)
-        }
-      }
-      
-      for (VAR in TRIPvars){
-        if (exists(VAR, where = PARKsem) == TRUE){
-          PARK_TripVars <- append(PARK_TripVars, VAR)
-        }
-      }
+source("~/SEM_Project/Script/getGroupVars.R")
       
 
 source("2_Cleaning.R")
@@ -90,50 +64,7 @@ for (VAR in PARK_SegmentVars){
 # local == 1 if zip matches local zipcodes or
 # local == 0 if zip matches non-local zipcodes
 
-
-PARK_localzip <- subset(PARKsegments, local == 1 & is.na(zip)==FALSE , select = zip)
-
-PARK_nonlocalzip <- subset(PARKsegments, local == 0 & is.na(zip)==FALSE , select = zip)
-
-IDs_local1 <- PARKbads[which(PARKbads$local_1 == 1), "ID"]
-          
-          # save for testing/verification
-          # IDs_local1 are observations where local == NA
-
-    # NOTE: try using dplyr "count" function rather than plry
-
-    PARK_localzip <-  count(PARK_localzip)
-        colnames(PARK_localzip) <- c("LOCAL_ZIP", "FREQUENCY")
-            n <- sum(PARK_localzip$FREQUENCY)
-            
-        LocalZip <- PARK_localzip[PARK_localzip$FREQUENCY/n > .05, "LOCAL_ZIP"]   
-                # CHANGE WEIGHT AFTER TESTING 
-
-    PARK_nonlocalzip <-  count(PARK_nonlocalzip)
-        colnames(PARK_nonlocalzip) <- c("NON-LOCAL_ZIP", "FREQUENCY")
-            m <- sum(PARK_nonlocalzip$FREQUENCY)
-        
-        nonLocalZip <- PARK_nonlocalzip[PARK_nonlocalzip$FREQUENCY/m > .0909, "NON-LOCAL_ZIP"]    
-      
-    ErrorZip <- intersect(LocalZip, nonLocalZip) 
-          ErrorZip <- append(ErrorZip, c("DK"))  # we can add other values as needed
-                # CHANGE WEIGHT AFTER TESTING 
-          
-                # NOTE: setdiff is the difference of set x net set y, not the union of 
-                # x & y net the intersection
-          
-        PARK_localzip <- setdiff(LocalZip, ErrorZip)
-        PARK_nonlocalzip <- setdiff(nonLocalZip, ErrorZip)
-        
-for (zips in PARK_localzip){
-  PARKsegments[which(PARKsegments$zip == zips & is.na(PARKsegments$local)), "local"] <- 1
-}
-  
-for (zips in PARK_nonlocalzip){
-  PARKsegments[which(PARKsegments$zip == zips & is.na(PARKsegments$local)), "local"] <- 0
-}
-        
-         # CHECK TO VERIFY THIS CODE IS WORKING
+source("~/SEM_Project/Script/clean_Local-Zip.R")
 
 # Now re-run script 2 to produce a second BADS matrix which reflects the corrected data frame
 
@@ -171,103 +102,72 @@ PARKbads_seg <- PARKbads_seg[-c(DropIDs),]
 ###########################################################################################
 #***************** Categorize Observations by Accomodation ********************************
 
-# Add columns to PARKsegments to classify each observation
-# Each column is initially a vector of zeros
-
-      # PARKsegments$day_local <- 0
-
-# SegmentVars <- c("nightsBackcountry", "nightsCampIn", "nightsCampOut",
-#                  "nightsLodgeIn", "nightsLodgeOut", "nightsCruise", "nightsOther")    
-
-# PARK_SegmentVars <- NULL
-
-# for (y in SegmentVars){
-#   if (exists(y, where = PARKsegments) == TRUE){
-    
-for (VAR in PARK_SegmentVars){
-  
-  # PARK_SegmentVars <- append(PARK_SegmentVars, y)   # vector of segments specific to PARK for use later
-  
-        v <- paste("ON", substring(VAR, 7), sep = "_")
-
+source("~/SEM_Project/Script/IdentifySegments.R")
       
-        # v <- paste("ON", substring(y, 7), sep = "_")
-        
-        # v <- paste(substring(y,7),
-        #            "nonlocal", sep = "_")   # This code reads:
-            # take the SegmentVar and remove the first six letter (e.g. "nights")
-            # then combine the remaining string with "_nonlocal" and store as variable "v"
-        
-        PARKsegments[,v] <- 0   # for each SegmentVar that is present in the PARKsegments data, create a column
-                                # named "v" and fill it with zeros
-  }
-# }
-
-# Identify local day trip and local overnight observations
-PARKsegments$day_local <- ifelse(PARKsegments$local == 1 & PARKsegments$overnight == 0, 1, 0)
-PARKsegments$day_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overnight == 0, 1,0)
-
-PARKsegments$overnight_local <- ifelse(PARKsegments$local == 1 & PARKsegments$overnight == 1, 1,0 )
-PARKsegments$overnight_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overnight == 1, 1 ,0)
-
-# Fill in zeros for overnights that only answered select accomodation categories (changing NA to zeros)
-        # NOTE: The assumption being made here is that if the respondent identified themselves as overnight and 
-        # identified at least one accomodation type for one or more nights, than any other accomodation types 
-        # unanswered should be zero (replace NA with zero)
-
-# THIS SHOULD BE MOVED TO RUN BEFORE SCRIPT 2 IS RUN AGAIN
-
+# # Add columns to PARKsegments to classify each observation
+# # Each column is initially a vector of zeros
+# 
 # for (VAR in PARK_SegmentVars){
-  # z <- paste(VAR, "1", sep = "_")
-  # 
-  #     IDfix <- PARKbads_seg[PARKbads_seg[,z]==1, "ID"]
-  #       # IDfix <- PARKbads[PARKbads[,z]==1, "ID"]
-  # 
-  #         PARKsegments[match(IDfix, PARKsegments$ID), VAR] <- 0
-# }   # FIX THIS NOT WORKING CORRECTLY
-
-# Categorize all nonlocal repondants by accomodation type. Create a vector of "ID"'s which are overnight 
-# but do not identify any type of accomodation type 
-
-IDs_badNights <- as.character(NULL) # Will capture "ID"s with incomplete data
-
-for (x in 1:length(PARKsegments$ID)){
-  if (as.integer(PARKsegments$overnight[x]) == 1){  # Confine loop to only overnight
-    
-
-          maxNightsType <- colnames(PARKsegments[c(x),PARK_SegmentVars])[max.col(PARKsegments[c(x),PARK_SegmentVars],
-                                                                                ties.method="first")]
-          # maxNightsType: vector specifying the type of accomodation with the largest number of nights provided
-
-              # NOTE: ties.method = "first" returns the first value in the vector, problem when all accomodation 
-              # types are equal (most likely all zeros)
-          
-          maxNights <- max(PARKsegments[c(x), PARK_SegmentVars])
-          minNights <- min(PARKsegments[c(x), PARK_SegmentVars])
-                # maxNights/minNights: vector specifying the largest/smallest integer value of nights given.
-          
-          if (is.na(maxNightsType) | 
-              as.character(maxNights) == 0 |
-              as.character(maxNights) == as.character(minNights)){ 
-                  IDs_badNights <- append(IDs_badNights, as.character(PARKsegments$ID[x])) 
-                  # IDs_badNights: vector of ID's which need to be addressed due to 
-                  # equal nights in multiple segment categories or missing data that 
-                  # was not caught by checks
-          }
-          
-          else { # If the observation is clean, categorize it accordingly
-              # v <- paste(substring(maxNightsType, 7), "nonlocal", sep = "_")
-                v <- paste("ON", substring(maxNightsType, 7), sep = "_")
-                  
-                PARKsegments[x,v] <- 1
-        }
-    }
-}
-
-if(length(IDs_badNights) > 0) stop(
-  "There are ID's which cannot be put into segments. Review IDs_badNights")
-# message <- function() warning("There are ID's which cannot be put into segments", call. = FALSE)
-# stop function() stopifnot(length(IDs_badNights) == 0)
+#   
+#         v <- paste("ON", substring(VAR, 7), sep = "_")
+# 
+#         PARKsegments[,v] <- 0   # for each SegmentVar that is present in the PARKsegments data, create a column
+#                                 # named "v" and fill it with zeros
+#   }
+# 
+# # Identify local day trip and local overnight observations
+# PARKsegments$day_local <- ifelse(PARKsegments$local == 1 & PARKsegments$overnight == 0, 1, 0)
+# PARKsegments$day_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overnight == 0, 1,0)
+# 
+# PARKsegments$overnight_local <- ifelse(PARKsegments$local == 1 & PARKsegments$overnight == 1, 1,0 )
+# PARKsegments$overnight_nonlocal <- ifelse(PARKsegments$local == 0 & PARKsegments$overnight == 1, 1 ,0)
+# 
+# # Fill in zeros for overnights that only answered select accomodation categories (changing NA to zeros)
+#         # NOTE: The assumption being made here is that if the respondent identified themselves as overnight and 
+#         # identified at least one accomodation type for one or more nights, than any other accomodation types 
+#         # unanswered should be zero (replace NA with zero)
+# 
+# 
+# # Categorize all nonlocal repondants by accomodation type. Create a vector of "ID"'s which are overnight 
+# # but do not identify any type of accomodation type 
+# 
+# IDs_badNights <- as.character(NULL) # Will capture "ID"s with incomplete data
+# 
+# for (x in 1:length(PARKsegments$ID)){
+#   if (as.integer(PARKsegments$overnight[x]) == 1){  # Confine loop to only overnight
+#     
+# 
+#           maxNightsType <- colnames(PARKsegments[c(x),PARK_SegmentVars])[max.col(PARKsegments[c(x),PARK_SegmentVars],
+#                                                                                 ties.method="first")]
+#           # maxNightsType: vector specifying the type of accomodation with the largest number of nights provided
+# 
+#               # NOTE: ties.method = "first" returns the first value in the vector, problem when all accomodation 
+#               # types are equal (most likely all zeros)
+#           
+#           maxNights <- max(PARKsegments[c(x), PARK_SegmentVars])
+#           minNights <- min(PARKsegments[c(x), PARK_SegmentVars])
+#                 # maxNights/minNights: vector specifying the largest/smallest integer value of nights given.
+#           
+#           if (is.na(maxNightsType) | 
+#               as.character(maxNights) == 0 |
+#               as.character(maxNights) == as.character(minNights)){ 
+#                   IDs_badNights <- append(IDs_badNights, as.character(PARKsegments$ID[x])) 
+#                   # IDs_badNights: vector of ID's which need to be addressed due to 
+#                   # equal nights in multiple segment categories or missing data that 
+#                   # was not caught by checks
+#           }
+#           
+#           else { # If the observation is clean, categorize it accordingly
+#               # v <- paste(substring(maxNightsType, 7), "nonlocal", sep = "_")
+#                 v <- paste("ON", substring(maxNightsType, 7), sep = "_")
+#                   
+#                 PARKsegments[x,v] <- 1
+#         }
+#     }
+# }
+# 
+# if(length(IDs_badNights) > 0) stop(
+#   "There are ID's which cannot be put into segments. Review IDs_badNights")
 
 ###########################################################################################
 #***************** Generate Segment Shares ************************************************
@@ -321,11 +221,4 @@ c <- NULL   # c: vector of observations by segment
 
 PARKshares_table <- data.frame(SEGMENT = a, SHARE = b, OBSERVATIONS = c)  
 
-
-###########################################################################################
-#***************** Generate Spending Profiles *********************************************
-    
-
-
-# write.csv(PARKsegments, "PARKsegments.csv", row.names = FALSE)
 
