9057af4ac04e8e2419f8f571b1b15810372fa4dd
CathyCThomas <ccullinanethomas@usgs.gov>
2016-07-18 13:50:32 -0600

    again

---
+++ Script/LengthOfStay.R
@@ -0,0 +1,6 @@
+#This script writes average length of stay estimates for each visitor segment
+#The script outputs the dataframe PARKlength_MEANS
+
+setwd("~/SEM_Project")
+
+source("4_Variable-Generation.R")
